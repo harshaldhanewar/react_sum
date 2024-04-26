50e57457e19e917fb741f351ef777b074c11d82c
Harshal Dhanewar <Harshal.Dhanewar@ril.com>
2024-04-26 13:25:02 +0530

    add task solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,22 @@
-// export const Sum = () => ();
+import { useEffect, useState } from 'react';
+
+export const Sum = props => {
+  const [number, setnumber] = useState({ a: 0, b: 0 });
+
+  useEffect(() => {
+    if (props) {
+      setnumber({
+        a: props.a || 0,
+        b: props.b || 0,
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <p>
+        Sum of {number.a} and {number.b} is {number.a + number.b}
+      </p>
+    </>
+  );
+};
